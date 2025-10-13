0846055e33c8a8322bd3e5df63b7de2445fa1737
Kodirov2samir <kodirovsamir174@gmail.com>
2025-10-13 06:42:20 +0500

    closure

--- workingWithFunctions/closureAndScope.js
+++ workingWithFunctions/closureAndScope.js
@@ -73,14 +73,43 @@ is an error, because even if js knows that hi exists for log(hi) hi is still uni
 //Step 2 fucntion declaration
 //function expression can be called before its initialization because whenever it is called it will be in ready state to function unlike let
 //the name of the function will be located in LexicalEnvironmnent and this name will be a ready function
-console.log(befort("hi"));
+befort("hi")
 function befort(hi) {
-  return hi
+  console.log(hi)
 }
 //hi
-i
-//it works like this: sees befort immediatelly sees that it is a function declaration and makes this  
-//other function types work the same wit variables
+
+//So here is the explanation:
+/*
+javascaript is both compiled and interpreted, in past it was just interpreted, but after they started using JIT(just in time) compilator
+so here is the order in which js works
+1)Parsing or preparement, checks the syntax if i had some probems maybe forgot the bracket and so on, builts AST(Abstract Syntax Tree) it is a structure of a programm, so in this level js understands where function variables are located but doesnt pay attention. in this level code is not interpreted it is just analyzed
+2)Compilation(creation phase)here lexicalEnvironment is used and everything is saved on the memory,all the variables and functions are registered, but let and const are in TDZ and they dont have value, unlike function declaration which is registered together with body, however the moment we call function dosnt work, dec() will be remembered as a variable
+3)Interpretation(execution phase)=> this is where value are given and functions expression start to work,console.log()also starts working in this level, so it expalins why function declaration has hoisting(able to be used before initilization) because when console.log(declaration) comes function ddeclaration will be already in the memory with the body or value, and the moment when function was called will work     
+*/
 
 //Note pure function is a function that doesnt use code from the outside
-//closure is when function rememners the variables from the outside
+//closure is when function remembers the variables from the outside and uses it 
+let names = "samir"
+dec(names)
+function dec(name) {
+  console.log(`hello i am ${name}`);
+}//hello i am samir
+//in this example dec is remembered with body but name will come in execution phase together with dec(names) so practically dec() will work in execution
+// fwrf(age)
+// let age = 23
+// function fwrf(age) {
+//   console.log(`i am ${age} years old`);
+// }//error because let age will be in tdz
+
+//step 3 outer and inner lexical Environment
+let step34 = "dar"
+function step3(name) {
+  console.log(`hello ${name} and ${step34}`);
+}
+step3("tre")
+//There are two lexical Environment
+//1)inner for inside of function console.log(`hello ${name} and ${step34}`);
+//2)outer or global
+//in inner the variable name is located and console.log(easily finds it), then outer place where let sterp34 and fucntion step3 are located, and it gains access to the step34 and shows on console
+//Note it will be searching untill it comes to the global lexical Environment and if it doesnt find the error will come 
