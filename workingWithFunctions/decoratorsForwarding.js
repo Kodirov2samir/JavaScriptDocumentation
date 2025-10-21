ac81770cc984428aecefdcfd4b33f078857d4df4
Kodirov2samir <kodirovsamir174@gmail.com>
2025-10-21 07:33:39 +0500

    func.call

--- workingWithFunctions/decoratorsForwarding.js
+++ workingWithFunctions/decoratorsForwarding.js
@@ -6,7 +6,7 @@ function slow(x) {
   return x;
 }
 
-function cachingDecorator(func) {
+function cachingDecorator(func) /* so here in the parameter the func authomatically makes the function slow work and then returns x */ {
   let cache = new Map();
 
   return function(x) {
@@ -28,3 +28,77 @@ console.log( "Again: " + slow(1) ); // slow(1) result returned from cache
 
 console.log( slow(2) ); // slow(2) is cached and the result returned
 console.log( "Again: " + slow(2) ); // slow(2) result returned from cache
+
+// this example we wrap a function info caching function which will prevent the extra usage of a function in case the result is the same
+
+//however this method doesnt work with object methods
+/*
+// сделаем worker.slow кеширующим
+let worker = {
+  someMethod() {
+    return 1;
+  },
+
+  slow(x) {
+    // здесь может быть страшно тяжёлая задача для процессора
+    alert("Called with " + x);
+    return x * this.someMethod(); // (*)
+  }
+};
+
+// тот же код, что и выше
+function cachingDecorator(func) {
+  let cache = new Map();
+  return function(x) {
+    if (cache.has(x)) {
+      return cache.get(x);
+    }
+    let result = func(x); // (**)
+    cache.set(x, result);
+    return result;
+  };
+}
+
+alert( worker.slow(1) ); // оригинальный метод работает
+
+worker.slow = cachingDecorator(worker.slow); // теперь сделаем его кеширующим
+
+alert( worker.slow(2) ); // Ой! Ошибка: не удаётся прочитать свойство 'someMethod' из 'undefined'
+*/
+
+// so here this.someMethod will be equal to undefined
+//to avoid this we have funt.call() property, in func.call(always what will be instead of this,and arguments) for example:
+
+function name(x){
+  console.log(`hello ${this.familyName} ${x}`);
+}
+let familyName = {familyName:"Qodirov"}
+name.call(familyName /* we literaly say put familyName instead of this it would look like this:familyName.familyName*/,"samir"/* here we are just continueing giving argumetns*/)
+//so to fix the example above we need to do 
+let worker = {
+  someMethod() {
+    return 1;
+  },
+
+  slow(x) {
+    console.log("Called with " + x);
+    return x * this.someMethod(); // (*)
+  }
+};
+
+function cachingDecorato(func) {
+  let cache = new Map();
+  return function(x) {
+    if (cache.has(x)) {
+      return cache.get(x);
+    }
+    let result = func.call(this, x); // by doing it we say keep original this
+    cache.set(x, result);
+    return result;
+  };
+}
+
+worker.slow = cachingDecorato(worker.slow); // теперь сделаем её кеширующей
+
+console.log( worker.slow(2) ); // работает
+console.log( worker.slow(2) ); // работает, не вызывая первоначальную функцию (кешируется)
